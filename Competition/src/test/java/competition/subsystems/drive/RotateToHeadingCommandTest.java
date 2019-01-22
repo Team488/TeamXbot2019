d8da3ac867acb93a677a93799cc43e667ff70dd9
John Gilbert <johngilb@gmail.com>
2019-01-21 16:02:59 -0800

    Working on basic rotation

---
+++ Competition/src/test/java/competition/subsystems/drive/RotateToHeadingCommandTest.java
@@ -0,0 +1,75 @@
+package competition.subsystems.drive;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.drive.commands.RotateToHeadingCommand;
+import competition.subsystems.pose.PoseSubsystem;
+
+public class RotateToHeadingCommandTest extends BaseCompetitionTest {
+
+    DriveSubsystem drive;
+    PoseSubsystem pose;
+    RotateToHeadingCommand command;
+
+    @Override
+    public void setUp() {
+        super.setUp();
+
+        drive = injector.getInstance(DriveSubsystem.class);
+        pose = injector.getInstance(PoseSubsystem.class);
+        command = injector.getInstance(RotateToHeadingCommand.class);
+    }
+
+    @Test
+    public void testInitialize() {
+        command.initialize();
+    }
+
+    @Test
+    public void testRotatesLeft() {
+        assertEquals(0, drive.rightMaster.getMotorOutputPercent(), 0.001);
+
+        command.setHeadingGoal(90, true);
+        command.initialize();
+        command.execute();
+
+        assertEquals(1, drive.rightMaster.getMotorOutputPercent(), 0.001);
+    }
+
+    @Test
+    public void testFinishes() {
+        assertEquals(0, drive.rightMaster.getMotorOutputPercent(), 0.001);
+        assertFalse(command.isFinished());
+
+        command.setHeadingGoal(90, true);
+        command.initialize();
+        command.execute();
+
+        assertFalse(command.isFinished());
+        assertEquals(1, drive.rightMaster.getMotorOutputPercent(), 0.001);
+        changeMockGyroHeading(90);
+
+        command.execute();
+        timer.advanceTimeInSecondsBy(3);
+        command.execute();
+
+        assertEquals(0, drive.rightMaster.getMotorOutputPercent(), 0.001);
+        assertTrue(command.isFinished());
+    }
+
+    protected void changeMockGyroHeading(double delta) {
+        double oldHeading = mockRobotIO.getGyroHeading();
+        double newHeading = oldHeading + delta;
+        setMockGyroHeading(newHeading);
+    }
+
+    protected void setMockGyroHeading(double heading) {
+        mockRobotIO.setGyroHeading(heading);
+    }
+
+}
