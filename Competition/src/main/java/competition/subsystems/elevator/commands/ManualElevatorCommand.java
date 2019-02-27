9532437f5a7c478420c52cde4c65ec244a6105b8
davidVespa <david.le.98118@gmail.com>
2019-02-26 18:59:47 -0800

    started manual elevator command

---
+++ Competition/src/main/java/competition/subsystems/elevator/commands/ManualElevatorCommand.java
@@ -0,0 +1,17 @@
+package competition.subsystems.elevator.commands;
+
+import xbot.common.command.BaseCommand;
+
+public class ManualElevatorCommand extends BaseCommand {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+}
